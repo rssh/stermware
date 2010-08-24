00b776c85bf5326418922a6131b64039a4ddfa1c
rssh <rssh@e6263924-d73c-0410-abf9-b8ad5fbdd136>
2010-08-24 06:17:33 +0000

    fixed error, where 0 was not accepted as INT
    git-svn-id: http://datacenter.gradsoft.ua/repos/termware/trunk-experimental/stermware@17098 e6263924-d73c-0410-abf9-b8ad5fbdd136

--- src/main/scala/ua/gradsoft/termware/parser/TermWareLexical.scala
+++ src/main/scala/ua/gradsoft/termware/parser/TermWareLexical.scala
@@ -33,7 +33,7 @@ class TermWareLexical(s:OperatorSyntax) extends Lexical
 
   def token: Parser[Token] = 
         token1 ^^ { 
-     (x) => { if (false) {System.out.println(x.tokenType+":"+x.chars); };  x }
+     (x) => { if (true) {System.out.println(x.tokenType+":"+x.chars); };  x }
   }
 
   def identifier: Parser[Token] = (
@@ -98,7 +98,7 @@ class TermWareLexical(s:OperatorSyntax) extends Lexical
   );
 
   def numberPrimitive:Parser[Token] = (
-     '0' ~! nonDecimalNumberPrimitive  ^^ { case x ~ y => y }
+     '0' ~ nonDecimalNumberPrimitive  ^^ { case x ~ y => y }
      |
       decimalNumberPrimitive
   );
