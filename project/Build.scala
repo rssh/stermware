a9ca0d0dcb2aa9fe9dc31c53215f551734799db1
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2011-11-21 13:17:46 +0300

    fix typo in sbt build

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,7 @@ object TermWareBuild extends Build
      organization:="ua.gradsoft",
      version:="0.5.0",
      scalaVersion:="2.9.1",
-     scalacOptions ++= Seq("-unchecked", "-deprecation")
+     scalacOptions ++= Seq("-unchecked", "-deprecation"),
      libraryDependencies += ("org.scalatest" %% "scalatest" % "1.6.1" % "test")
    );
 
