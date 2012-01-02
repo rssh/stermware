f6c215d5ecf52c51d94d08c26449904636a134fb
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2012-01-02 12:27:34 +0200

    created fun suite for let-s.

---
+++ src/test/scala/ua/gradsoft/termware/LetTermFunSuite.scala
@@ -0,0 +1,24 @@
+package ua.gradsoft.termware;
+
+import org.scalatest.FunSuite;
+
+class LetTermFunSuite extends FunSuite {
+
+  test("let term must be able to be created from assignmetns and main term") {
+    pending; 
+  }
+
+  test("internal variable in internal let must hide one in external let") {
+    pending;
+  }
+
+  test("substitution on let must be anoter let") {
+    pending;
+  }
+
+  test("substitution on let must double internal let's") {
+    pending;
+  }
+
+
+}
