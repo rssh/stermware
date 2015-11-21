73d18cfb9c7709981dde39dfa35f876cab87c668
Ruslan Shevchenko <rssh@rssh3.local>
2015-11-21 20:20:58 +0200

    propagate termSystem and attributes throught all term types.

--- src/main/scala/termware/Term.scala
+++ src/main/scala/termware/Term.scala
@@ -7,45 +7,64 @@ sealed trait Term extends TermOps
  val termSystem: TermSystem = FreeTermSystem
 }
 
-class AtomTerm(value:String, 
+case class AtomTerm(value:String, 
                override val attributes: Map[Name,Term] = Map(), 
-               override val termSystem: TermSystem = FreeTermSystem) extends Term with AtomTermOps
+               override val termSystem: TermSystem = FreeTermSystem
+                   ) extends Term with AtomTermOps
 {
-  val name = AtomName(value)
+  override val name = AtomName(value)
 }
 
 sealed trait PrimitiveTerm extends Term with PrimitiveTermOps 
 
-class StringTerm(value:String) extends PrimitiveTerm with StringTermOps
+case class StringTerm(value:String,
+               override val attributes: Map[Name,Term] = Map(), 
+               override val termSystem: TermSystem = FreeTermSystem
+                     ) extends PrimitiveTerm with StringTermOps
 {
-  val name = StringName(value)
+  override val name = StringName(value)
 }
 
-class CharTerm(value:Character) extends PrimitiveTerm with CharTermOps
+case class CharTerm(value:Character,
+               override val attributes: Map[Name,Term] = Map(), 
+               override val termSystem: TermSystem = FreeTermSystem
+                   ) extends PrimitiveTerm with CharTermOps
 {
- val name = CharName(value)
+ override val name = CharName(value)
 }
 
 sealed trait NumericTerm extends PrimitiveTerm with NumericTermOps
 
-class Int32Term(value:Int) extends NumericTerm with Int32TermOps
+case class Int32Term(value:Int,
+                override val attributes: Map[Name,Term] = Map(), 
+                override val termSystem: TermSystem = FreeTermSystem
+               ) extends NumericTerm with Int32TermOps
 {
-  val name = IntName(value)
+  override val name = IntName(value)
 }
 
-class Int64Term(value: Long) extends NumericTerm with Int64TermOps
+case class Int64Term(value: Long,
+                override val attributes: Map[Name,Term] = Map(), 
+                override val termSystem: TermSystem = FreeTermSystem
+               ) extends NumericTerm with Int64TermOps
 {
-  val name = LongName(value)
+  override val name = LongName(value)
 }
 
-class DoubleTerm(value: Double) extends NumericTerm with DoubleTermOps
+case class DoubleTerm(value: Double,
+                 override val attributes: Map[Name,Term] = Map(), 
+                 override val termSystem: TermSystem = FreeTermSystem
+                ) extends NumericTerm with DoubleTermOps
 {
-  val name = DoubleName(value)
+  override val name = DoubleName(value)
 }
 
-case class OpaqueTerm(value: Array[Byte]) extends PrimitiveTerm with OpaqueTermOps
+case class OpaqueTerm(value: Array[Byte],
+                 override val attributes: Map[Name,Term] = Map(), 
+                 override val termSystem: TermSystem = FreeTermSystem
+                     ) extends PrimitiveTerm with OpaqueTermOps
 {
-  val name = OpaqueName(value)
+  override val name = OpaqueName(value)
 }
 
 case class StructuredTerm(termStructure: TermStructure, 
@@ -55,7 +74,7 @@ case class StructuredTerm(termStructure: TermStructure,
                           override val termSystem: TermSystem = FreeTermSystem
                           ) extends Term with StructuredTermOps
 
-class VarTerm(val name: Name,
+case class VarTerm(val name: Name,
               override val scope: Option[Term] = None,
               override val attributes: Map[Name,Term] = Map(), 
               override val termSystem: TermSystem = FreeTermSystem
